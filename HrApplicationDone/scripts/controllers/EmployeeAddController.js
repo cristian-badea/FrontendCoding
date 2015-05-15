e6ba96a3ff89e70de041ee0aae2ed3a8fd82b7d9
Cristian_Badea <badea_cristian_alexandru@yahoo.com>
2015-05-15 14:59:50 +0300

    Hr Application Finished
    Employee ( add, view, edit, delete )

---
+++ HrApplicationDone/scripts/controllers/EmployeeAddController.js
@@ -0,0 +1,51 @@
+hrApp.controller('EmployeeAddController', ['$scope', '$http', '$location', 'commonResourcesFactory',
+    function ($scope, $http, $location, $commonResourcesFactory) {
+    $scope.employee = {};
+    $scope.requiredErrorMessage = "Please fill out this form!";
+    $scope.patternDateNotRespectedMessage = "The date format should be yyyy-mm-dd";
+    $scope.patternCommisionNotRespectedMessage = "Commission should be in the format 0.XX";
+
+    //TODO#HR1
+    $scope.jobs;
+    $scope.departments;
+    $scope.managers;
+
+   $http.get($commonResourcesFactory.findAllJobsUrl)
+       .success(function(data)
+        {
+            $scope.jobs = data;
+        });
+    $http.get($commonResourcesFactory.findAllDepartmentsUrl)
+        .success(function(data)
+        {
+            $scope.departments = data;
+        });
+    $http.get($commonResourcesFactory.findAllEmployeesUrl)
+        .success(function(data) {
+            $scope.managers = data;
+        });
+
+
+    /**
+     * Reset form
+     */
+    $scope.reset = function () {
+        this.employee = {};
+    };
+
+
+    /**
+     * Persist an employee
+     * @param addEmployee - employee to be persisted
+     */
+    $scope.create = function (addEmployee) {
+        $http({url: $commonResourcesFactory.addEmployeeUrl, method: 'POST',data:addEmployee}).
+            success(function (data) {
+                $scope.employee = data;
+                $location.url('/employeeview/'+$scope.employee.employeeId);
+            });
+    }
+
+    $scope.datePattern = /^\d{4}-\d{2}-\d{2}$/;
+    $scope.commissionPattern = /^[0]\.\d{1}(\d)?$/;
+}]);
